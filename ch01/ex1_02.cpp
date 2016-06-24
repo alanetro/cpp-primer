b63632130ad808edb8647a35ef0784634a5983ed
alanetro <cardosoalan@yahoo.com.br>
2016-06-24 19:43:11 +0000

    Added ex1_02.cpp

---
+++ ch01/ex1_02.cpp
@@ -0,0 +1,16 @@
+int main()
+{
+    return -1;
+}
+
+/**
+ * $ g++ ex1_02.cpp
+ * $ ./a.out
+ * $ echo $?
+ * $ 255
+ * 
+ * exit takes only integer args in the range 0 - 255, so we entered -1 and -1 is out of range.
+ * Can and sometimes will be different, depending on your operating system.
+ * */
+ 
+ 
